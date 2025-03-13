7622b910e0a5db74b654b03f390dcf458254ede7
Roak kennady <roakkennady17@gmail.com>
2025-03-13 18:43:45 +0500

    removed the unwanted route

--- server/services/bulkUploadService.js
+++ server/services/bulkUploadService.js
@@ -58,7 +58,7 @@ const forgotPassword = async (email) => {
         await user.save();
 
         // Send the reset token via email
-        const resetUrl = `https://mdnrpt.medianet.mv/bulk-uploads/reset-password/${resetToken}`;
+        const resetUrl = `https://mdnrpt.medianet.mv/reset-password/${resetToken}`;
 
         console.log(process.env.EMAIL_USER,process.env.EMAIL_PASS,'email user and pass')
 
