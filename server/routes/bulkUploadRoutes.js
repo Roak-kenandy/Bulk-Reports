f7d78417de10f42c6ce95ff13f01cd5e758edbff
Roak kennady <roakkennady17@gmail.com>
2025-03-16 10:34:40 +0500

    Reloaded route added

--- server/routes/bulkUploadRoutes.js
+++ server/routes/bulkUploadRoutes.js
@@ -5,6 +5,10 @@ const bulkUploadController = require('../controller/bulkUploadController');
 
 const upload = multer({ dest: 'uploads/' });
 
+router.get('/', (req, res) => {
+    res.json({ status: 'Bulk uploads endpoint active' });
+  });
+
 router.post('/login', bulkUploadController.loginUser);
 
 router.post('/forgot-password', bulkUploadController.forgotPassword);
